4b2056514a5b9f285036e312dddb37b1e89e3d9d
Das <DASP2@TDBFG.COM>
2017-04-03 20:12:38 -0400

    Added fingerprint detection

--- eVault/app/src/main/java/com/poc/evault/activities/HomeActivity.java
+++ eVault/app/src/main/java/com/poc/evault/activities/HomeActivity.java
@@ -1,18 +1,37 @@
 package com.poc.evault.activities;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Dialog;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.Vibrator;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -23,41 +42,61 @@ import com.google.android.gms.common.api.Status;
 import com.poc.evault.R;
 import com.poc.evault.utils.SharedPreferenceUtil;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by DASP2 on 4/1/2017.
  */
+@TargetApi(Build.VERSION_CODES.M)
 public class HomeActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
     private GoogleApiClient mGoogleApiClient;
+    private KeyStore keyStore;
+    private static final String KEY_NAME = "androidHive";
+    private Cipher cipher;
+    private Dialog dialog;
+    private boolean isAuthenticationRequired = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        String name="";
-        String email="";
-        String photoUrl="";
-        Bundle bundle=getIntent().getExtras();
-        if(bundle!=null){
-            name=bundle.getString("NAME");
-            email=bundle.getString("EMAIL");
-            photoUrl=bundle.getString("URL");
+        String name = "";
+        String email = "";
+        String photoUrl = "";
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            name = bundle.getString("NAME");
+            email = bundle.getString("EMAIL");
+            photoUrl = bundle.getString("URL");
         }
 
-        ImageView imgAdd=(ImageView)findViewById(R.id.fab);
-        ImageView imgLogout=(ImageView)findViewById(R.id.logout);
-        TextView txtName=(TextView)findViewById(R.id.name);
+        ImageView imgAdd = (ImageView) findViewById(R.id.fab);
+        ImageView imgLogout = (ImageView) findViewById(R.id.logout);
+        TextView txtName = (TextView) findViewById(R.id.name);
         txtName.setText(name);
-        TextView txtEmail=(TextView)findViewById(R.id.email);
+        TextView txtEmail = (TextView) findViewById(R.id.email);
         txtEmail.setText(email);
-        CircleImageView imgUserPhoto=(CircleImageView)findViewById(R.id.user_photo);
+        CircleImageView imgUserPhoto = (CircleImageView) findViewById(R.id.user_photo);
 
-        DownloadImageTask task=new DownloadImageTask(imgUserPhoto);
+        DownloadImageTask task = new DownloadImageTask(imgUserPhoto);
         task.execute(photoUrl);
 
         imgAdd.setOnClickListener(this);
@@ -72,19 +111,23 @@ public class HomeActivity extends AppCompatActivity implements
 
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this )
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
+        generateKey();
+        cipherInit();
     }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.fab:
+                initFingerprint();
+                showDialog();
                 break;
             case R.id.logout:
                 signOut();
-                startActivity(new Intent(HomeActivity.this,SplashActivity.class));
+                startActivity(new Intent(HomeActivity.this, SplashActivity.class));
                 finish();
                 break;
         }
@@ -97,6 +140,7 @@ public class HomeActivity extends AppCompatActivity implements
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
+
         public DownloadImageTask(ImageView bmImage) {
             this.bmImage = bmImage;
         }
@@ -125,5 +169,164 @@ public class HomeActivity extends AppCompatActivity implements
                     public void onResult(Status status) {
                     }
                 });
+    }
+
+    public void showDialog() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_fingerprint);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                isAuthenticationRequired = false;
+            }
+        });
+    }
+
+    private void initFingerprint() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        FingerprintManager fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
+        if (!fingerprintManager.isHardwareDetected()) {
+            Toast.makeText(this, "Your Device does not have a Fingerprint Sensor", Toast.LENGTH_LONG).show();
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Fingerprint authentication permission not enabled", Toast.LENGTH_LONG).show();
+            } else {
+                if (!fingerprintManager.hasEnrolledFingerprints()) {
+                    Toast.makeText(this, "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show();
+                } else {
+                    if (!keyguardManager.isKeyguardSecure()) {
+                        Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_LONG).show();
+                    } else {
+                        isAuthenticationRequired = true;
+                        FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                           /* FingerprintHandler helper = new FingerprintHandler(this);
+                            helper.startAuth(fingerprintManager, cryptoObject);*/
+                        fingerprintManager.authenticate(cryptoObject, new CancellationSignal(), 0, authenticationCallback, null);
+                    }
+                }
+            }
+        }
+    }
+
+    private FingerprintManager.AuthenticationCallback authenticationCallback = new FingerprintManager.AuthenticationCallback() {
+        @Override
+        public void onAuthenticationError(int errorCode, CharSequence errString) {
+            animateFingerprintDialog();
+        }
+
+        @Override
+        public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+            animateFingerprintDialog();
+        }
+
+        @Override
+        public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+            if (isAuthenticationRequired) {
+                dialog.dismiss();
+                isAuthenticationRequired = false;
+
+            }
+        }
+
+        @Override
+        public void onAuthenticationFailed() {
+            animateFingerprintDialog();
+        }
+    };
+
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        KeyGenerator keyGenerator;
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException("Failed to get KeyGenerator instance", e);
+        }
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException |
+                InvalidAlgorithmParameterException
+                | CertificateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+
+    private void startVibration() {
+        ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE)).vibrate(500);
+    }
+
+    private void startShakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.animation_shake);
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //clearPasscodeFields();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        ((ViewGroup) dialog.getWindow().getDecorView())
+                .getChildAt(0).startAnimation(shake);
+    }
+
+    private void animateFingerprintDialog(){
+        startShakeAnimation();
+        startVibration();
+        Toast.makeText(this, "Try again", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        super.onBackPressed();
     }
 }
